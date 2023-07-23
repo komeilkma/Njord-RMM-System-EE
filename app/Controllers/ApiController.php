1d49e9a33a52b16e571a57294d00541da62e7a85
Komeil Majidi <k.majidi@esmcell.com>
2023-07-24 02:22:11 +0330

    test api and authenticateUser API #18

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,51 @@
+<?php
+// API Version : 1.0.0
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+use Firebase\JWT\JWT;
+
+class ApiController extends ResourceController
+{
+    public function getData($id)
+    {
+        $data = [
+            'id' => $id,
+            'check' => 'test_api',
+        ];
+
+        return $this->respond($data);
+    }
+
+    public function authenticateUser()
+    {
+        $data = $this->request->getJSON();
+
+        
+        $db = \Config\Database::connect();
+        $db->connect();
+        $query = $db->table('users')->where('username', $data->username)->get();
+        if ($query->getNumRows() === 1) {
+            $user = $query->getRow();
+            $hashedPassword = $user->password;
+
+            if (password_verify($data->password, $hashedPassword)) {
+                $key = getenv('api.jwt.secret');
+                $payload = [
+                    'username' => $data->username,
+                ];
+                $token = JWT::encode($payload, $key, 'HS256');
+                return $this->respond(['user' => $data->username, 'token' => $token]);
+            }else {
+
+                return $this->failUnauthorized("username or password is incorrect");
+            }
+        }else {
+          
+            return $this->failNotFound("profile was not found");
+
+        }
+
+    }
+    
+}
